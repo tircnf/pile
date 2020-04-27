49f0f4df2fa9bd4fb3b9b32f1ba54ee7913a8741
tircnf <tircnf+github@gmail.com>
2020-04-27 14:30:37 -0500

    tweaks

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App hosted on Render.com"/>
   </div>
 </template>
 
