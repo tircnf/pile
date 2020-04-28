0b6b5ba66d16f73fe66c60def830cb0689732245
mburns <mike.burns@afoundria.com>
2020-04-28 13:52:23 -0500

    Hook the sample up the Marvel API.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App hosted on Render.com.  Hi Mike Kevin Erich and Byron."/>
-  </div>
+    <div>
+
+        <div id="app">
+            <img alt="Vue logo" src="./assets/logo.png">
+        </div>
+        <HelloWorld msg="Welcome to Your Vue.js App hosted on Render.com.  Hi Mike Kevin Erich and Byron."/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            HelloWorld
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,96 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+
+        <label>Enter the name for a 'starts with' match
+            <input v-model="name" @keyup.enter="search">
+        </label>
+        <button @click="search">Search</button>
+
+
+
+
+        <ul>
+            <li v-if="result.attributionHTML"><span v-html="result.attributionHTML"/>
+               <ul>
+                   <li>Showing results {{result.data.offset}} to {{result.data.offset + result.data.count}} of {{result.data.total}}</li>
+               </ul>
+            </li>
+            <li :key="character.id" v-for="character in result.data.results">
+                <img :src="`${character.thumbnail.path}/portrait_small.jpg`" alt="thumbnail image" >
+                {{character.id}} - {{character.name}}
+                <p><span v-html="character.description"/></p>
+                <ul>
+                    <li> Comics: {{character.comics.available}}</li>
+                    <li> Series: {{character.series.available}}</li>
+                    <li> Stories: {{character.stories.available}}</li>
+                    <li> events: {{character.events.available}}</li>
+                    <li v-for="url in character.urls" :key="url.type">
+                        {{url.type}}   <a :href="url.url" target="_blank">{{url.url}}</a>
+                    </li>
+                </ul>
+<!--                <pre>{{character}}</pre>-->
+            </li>
+        </ul>
+
+<!--        <pre>{{result}}</pre>-->
+
+
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        },
+        data() {
+            return {
+                one: "two",
+                apiKey: "b51a816f55c2b752fe029e625cd2d81b",
+                endpoint: "https://gateway.marvel.com",
+                name: "Spider",
+                result: {
+                    data: {
+                        results: []
+                    }
+                }
+            }
+        },
+        methods: {
+            search() {
+
+                if (! this.name) {
+                    return;
+                }
+                const url = new URL(`${this.endpoint}/v1/public/characters`);
+                const params = {
+                    nameStartsWith: this.name,
+                    apikey: this.apiKey
+                };
+
+                //https://gateway.marvel.com/v1/public/characters?nameStartsWith=spider&apikey=b51a816f55c2b752fe029e625cd2d81b
+                Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+
+                fetch(url)
+                    .then(response => response.json())
+                    .then(json => this.result = json);
+
+                this.name="";
+            }
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+
+
+    a {
+        color: #42b983;
+    }
 </style>
