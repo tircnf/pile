c14a1575e036807c9afa00e891f4635b7afb711f
mburns <mike.burns@afoundria.com>
2020-04-29 14:03:18 -0500

    UI changes.  Make the lists look a bit better.
    tooltips used to show larger versions of images.
    The initial display of the tooltip is usually in the wrong place.
    I think because the image isn't loaded yet.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -40,9 +40,10 @@
 
             <v-col cols="6">
 
-                <h3>Characters <v-icon v-if="characterResults.loading">mdi-loading mdi-spin</v-icon> </h3>
+                <h3>Characters
+                    <v-icon v-if="characterResults.loading">mdi-loading mdi-spin</v-icon>
+                </h3>
                 <h5>{{characterSearchMessage}}</h5>
-
                 <ul>
                     <li v-if="characterResults.attributionHTML"><span v-html="characterResults.attributionHTML"/>
                         <ul>
@@ -56,94 +57,102 @@
                         </ul>
                     </li>
                     <li :key="character.id" v-for="(character,index) in characterResults.data.results">
-                        <!--suppress HtmlUnknownTarget -->
-                        {{index+1}} &mdash;
-                        {{character.id}} - {{character.name}}
-                        <v-tooltip right>
+                        <v-tooltip open-delay="350"
+                                   close-delay="100"
+                                   right
+                        >
                             <template v-slot:activator="{ on }">
                                 <!--suppress HtmlUnknownTarget -->
                                 <v-img
-                                        v-on="on"
+                                        :height="thumbnailSize.height"
                                         :src="`${character.thumbnail.path}/${thumbnailSize.name}.jpg`"
                                         :width="thumbnailSize.width"
-                                        :height="thumbnailSize.height"
-                                        alt="thumbnail image"/>
+                                        alt="thumbnail image"
+                                        class="float-left mr-4"
+                                        v-on="on"
+
+                                />
                             </template>
                             <!--suppress HtmlUnknownTarget -->
                             <v-img
                                     :src="`${character.thumbnail.path}.jpg`"
-                                    max-height="750"
-                                    alt="thumbnail image"/>
+                                    alt="thumbnail image"
+                                    max-height="750"/>
 
 
                         </v-tooltip>
-                        <p><span v-html="character.description"/></p>
-                        <ul>
-                            <li> Comics: {{character.comics.available}}</li>
-                            <li> Series: {{character.series.available}}</li>
-                            <li> Stories: {{character.stories.available}}</li>
-                            <li> events: {{character.events.available}}</li>
-                            <li :key="url.type" v-for="url in character.urls">
-                                <a :href="url.url" target="_blank">{{url.type}}</a>
-                            </li>
-                        </ul>
-                        <!--                <pre>{{character}}</pre>-->
+
+                        <div style="min-height: 175px;">
+
+                            <!--                        {{character.id}} - -->
+                            <h5>{{index+1}} {{character.name}}</h5>
+                            <ul>
+                                <li> Comics: {{character.comics.available}}</li>
+                                <li> Series: {{character.series.available}}</li>
+                            </ul>
+                            <p><span v-html="character.description"/></p>
+                            <!--                <pre>{{character}}</pre>-->
+                        </div>
                     </li>
                 </ul>
             </v-col>
 
             <v-col cols="6">
-                <h3>Series  <v-icon v-if="seriesResults.loading">mdi-loading mdi-spin</v-icon></h3>
+                <h3>Series
+                    <v-icon v-if="seriesResults.loading">mdi-loading mdi-spin</v-icon>
+                </h3>
                 <h5>{{seriesSearchMessage}}</h5>
                 <ul>
                     <li v-if="seriesResults.attributionHTML"><span v-html="seriesResults.attributionHTML"/>
                         <ul>
-                            <li v-if="seriesResults.data.count">Showing characterResults {{seriesResults.data.offset+1}}
-                                to {{seriesResults.data.offset + seriesResults.data.count}}
+                            <li v-if="seriesResults.data.count">Showing seriesResults
+                                {{seriesResults.data.offset+1}} to {{seriesResults.data.offset +
+                                seriesResults.data.count}}
                                 of
                                 {{seriesResults.data.total}}
                             </li>
                             <li v-else>No Results found</li>
                         </ul>
                     </li>
-                    <li :key="comic.id" v-for="(comic,index) in seriesResults.data.results">
-                        <!--suppress HtmlUnknownTarget -->
-                        {{index+1}} &mdash;
-                        {{comic.id}} - {{comic.title}}
-
-                        <v-tooltip right>
+                    <li :key="series.id" v-for="(series,index) in seriesResults.data.results">
+                        <v-tooltip open-delay="350"
+                                   close-delay="100"
+                                   left
+                        >
                             <template v-slot:activator="{ on }">
                                 <!--suppress HtmlUnknownTarget -->
                                 <v-img
-                                       v-on="on"
-                                       :src="`${comic.thumbnail.path}/${thumbnailSize.name}.jpg`"
-                                       :width="thumbnailSize.width"
-                                       :height="thumbnailSize.height"
-                                       alt="thumbnail image"/>
+                                        :height="thumbnailSize.height"
+                                        :src="`${series.thumbnail.path}/${thumbnailSize.name}.jpg`"
+                                        :width="thumbnailSize.width"
+                                        alt="thumbnail image"
+                                        class="float-left mr-4"
+                                        v-on="on"
+
+                                />
                             </template>
                             <!--suppress HtmlUnknownTarget -->
                             <v-img
-                                   :src="`${comic.thumbnail.path}.jpg`"
-                                   max-height="750"
-                                   alt="thumbnail image"/>
+                                    :src="`${series.thumbnail.path}.jpg`"
+                                    alt="thumbnail image"
+                                    max-height="750"/>
 
 
                         </v-tooltip>
 
-                        <p><span v-html="comic.description"/></p>
-                        <ul>
-                            <li> Comics: {{comic.comics.available}}</li>
-                            <li> Characters: {{comic.characters.available}}</li>
-                            <li> Stories: {{comic.stories.available}}</li>
-                            <li> events: {{comic.events.available}}</li>
-                            <li :key="url.type" v-for="url in comic.urls">
-                                <a :href="url.url" target="_blank">{{url.type}} </a>
-                            </li>
-                        </ul>
-                        <!--                                        <pre>{{comic}}</pre>-->
+                        <div style="min-height: 175px;">
+
+                            <!--                        {{series.id}} - -->
+                            <h5>{{index+1}} {{series.title}}</h5>
+                            <ul>
+                                <li> Comics: {{series.comics.available}}</li>
+                                <li> Characters: {{series.characters.available}}</li>
+                            </ul>
+                            <p><span v-html="series.description"/></p>
+                            <!--                <pre>{{series}}</pre>-->
+                        </div>
                     </li>
                 </ul>
-
             </v-col>
 
         </v-row>
@@ -217,7 +226,7 @@
                     }
                 };
 
-                this.seriesSearchMessage="";
+                this.seriesSearchMessage = "";
                 this.seriesResults = {
                     loading: true,
                     data: {
@@ -228,10 +237,10 @@
 
                 this.searchCharacters(this.name).then(json => {
 
-                    console.log("response = ",json);
-                    if (json.code===409) {
-                        this.seriesResults.loading=false;
-                        this.seriesSearchMessage=json.status;
+                    console.log("response = ", json);
+                    if (json.code === 409) {
+                        this.seriesResults.loading = false;
+                        this.seriesSearchMessage = json.status;
                         return;
                     }
                     //console.log("Search Characters returned ", json);
@@ -239,9 +248,9 @@
                 });
 
                 this.searchSeries(this.name).then(json => {
-                    if (json.code===409) {
-                        this.characterResults.loading=false;
-                        this.characterSearchMessage=json.status;
+                    if (json.code === 409) {
+                        this.characterResults.loading = false;
+                        this.characterSearchMessage = json.status;
                         return;
                     }
                     //console.log("Search Comics returned ", json);
