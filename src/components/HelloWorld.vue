69b88fc6a0660323f03f34c53119b8115bbf4402
mburns <mike.burns@afoundria.com>
2020-04-28 22:51:19 -0500

    Search series too.
    Lots of playing with images.  How to hover over
    an image to see a bigger version?
    
    Using v-hover doesn't quite work correctly.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,9 +26,10 @@
                 />
             </v-col>
             <v-col cols="2">
-                <v-btn class="primary"
-                       @click="search"
-                >Search</v-btn>
+                <v-btn @click="search"
+                       class="primary"
+                >Search
+                </v-btn>
             </v-col>
 
 
@@ -40,20 +41,25 @@
 
                 <h3>Characters</h3>
                 <ul>
-                    <li v-if="result.attributionHTML"><span v-html="result.attributionHTML"/>
+                    <li v-if="characterResults.attributionHTML"><span v-html="characterResults.attributionHTML"/>
                         <ul>
-                            <li v-if="result.data.count">Showing results {{result.data.offset+1}} to {{result.data.offset + result.data.count}}
+                            <li v-if="characterResults.data.count">Showing characterResults
+                                {{characterResults.data.offset+1}} to {{characterResults.data.offset +
+                                characterResults.data.count}}
                                 of
-                                {{result.data.total}}
+                                {{characterResults.data.total}}
                             </li>
                             <li v-else>No Results found</li>
                         </ul>
                     </li>
-                    <li :key="character.id" v-for="(character,index) in result.data.results">
+                    <li :key="character.id" v-for="(character,index) in characterResults.data.results">
                         <!--suppress HtmlUnknownTarget -->
                         {{index+1}} &mdash;
-                        <img :src="`${character.thumbnail.path}/portrait_small.jpg`" alt="thumbnail image">
                         {{character.id}} - {{character.name}}
+                        <v-img :height="thumbnailSize.height"
+                               :src="`${character.thumbnail.path}/${thumbnailSize.name}.jpg`"
+                               :width="thumbnailSize.width"
+                               alt="thumbnail image"/>
                         <p><span v-html="character.description"/></p>
                         <ul>
                             <li> Comics: {{character.comics.available}}</li>
@@ -61,7 +67,7 @@
                             <li> Stories: {{character.stories.available}}</li>
                             <li> events: {{character.events.available}}</li>
                             <li :key="url.type" v-for="url in character.urls">
-                                {{url.type}} <a :href="url.url" target="_blank">{{url.url}}</a>
+                                <a :href="url.url" target="_blank">{{url.type}}</a>
                             </li>
                         </ul>
                         <!--                <pre>{{character}}</pre>-->
@@ -70,34 +76,56 @@
             </v-col>
 
             <v-col cols="3">
-                <h3>Comics</h3>
+                <h3>Series</h3>
                 <ul>
-                    <li v-if="result.attributionHTML"><span v-html="result.attributionHTML"/>
+                    <li v-if="comicResults.attributionHTML"><span v-html="comicResults.attributionHTML"/>
                         <ul>
-                            <li v-if="result.data.count">Showing results {{result.data.offset+1}} to {{result.data.offset + result.data.count}}
+                            <li v-if="comicResults.data.count">Showing characterResults {{comicResults.data.offset+1}}
+                                to {{comicResults.data.offset + comicResults.data.count}}
                                 of
-                                {{result.data.total}}
+                                {{comicResults.data.total}}
                             </li>
                             <li v-else>No Results found</li>
                         </ul>
                     </li>
-                    <li :key="character.id" v-for="character in result.data.results">
+                    <li :key="comic.id" v-for="(comic,index) in comicResults.data.results">
                         <!--suppress HtmlUnknownTarget -->
-                        <img :src="`${character.thumbnail.path}/portrait_small.jpg`" alt="thumbnail image">
-                        {{character.id}} - {{character.name}}
-                        <p><span v-html="character.description"/></p>
+                        {{index+1}} &mdash;
+                        {{comic.id}} - {{comic.title}}
+
+                        <v-tooltip right>
+                            <template v-slot:activator="{ on }">
+                                <!--suppress HtmlUnknownTarget -->
+                                <v-img
+                                       v-on="on"
+                                       :src="`${comic.thumbnail.path}/${thumbnailSize.name}.jpg`"
+                                       :width="thumbnailSize.width"
+                                       :height="thumbnailSize.height"
+                                       alt="thumbnail image"/>
+                            </template>
+                            <!--suppress HtmlUnknownTarget -->
+                            <v-img
+                                   :src="`${comic.thumbnail.path}.jpg`"
+                                   max-height="750"
+                                   alt="thumbnail image"/>
+
+
+                        </v-tooltip>
+
+                        <p><span v-html="comic.description"/></p>
                         <ul>
-                            <li> Comics: {{character.comics.available}}</li>
-                            <li> Series: {{character.series.available}}</li>
-                            <li> Stories: {{character.stories.available}}</li>
-                            <li> events: {{character.events.available}}</li>
-                            <li :key="url.type" v-for="url in character.urls">
-                                {{url.type}} <a :href="url.url" target="_blank">{{url.url}}</a>
+                            <li> Comics: {{comic.comics.available}}</li>
+                            <li> Characters: {{comic.characters.available}}</li>
+                            <li> Stories: {{comic.stories.available}}</li>
+                            <li> events: {{comic.events.available}}</li>
+                            <li :key="url.type" v-for="url in comic.urls">
+                                <a :href="url.url" target="_blank">{{url.type}} </a>
                             </li>
                         </ul>
-                        <!--                <pre>{{character}}</pre>-->
+                        <!--                                        <pre>{{comic}}</pre>-->
                     </li>
                 </ul>
+
             </v-col>
 
             <v-col cols="3">
@@ -155,22 +183,56 @@
             msg: String
         },
         methods: {
-            search() {
-                if (!this.name) {
-                    return;
-                }
+            searchCharacters(searchStr) {
                 const url = new URL(`${this.endpoint}/v1/public/characters`);
                 const params = {
-                    nameStartsWith: this.name,
+                    nameStartsWith: searchStr,
                     apikey: this.apiKey
                 };
-
                 //https://gateway.marvel.com/v1/public/characters?nameStartsWith=spider&apikey=b51a816f55c2b752fe029e625cd2d81b
                 Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
 
-                fetch(url)
+                return fetch(url)
                     .then(response => response.json())
-                    .then(json => this.result = json);
+            },
+
+            searchSeries(searchStr) {
+                const url = new URL(`${this.endpoint}/v1/public/series`);
+                const params = {
+                    titleStartsWith: searchStr,
+                    apikey: this.apiKey
+                };
+                //https://gateway.marvel.com/v1/public/characters?nameStartsWith=spider&apikey=b51a816f55c2b752fe029e625cd2d81b
+                Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+
+                return fetch(url)
+                    .then(response => response.json())
+            },
+
+            search() {
+                this.characterResults = {
+                    data: {
+                        results: []
+                    }
+                };
+                this.comicResults = {
+                    data: {
+                        results: []
+                    }
+                };
+
+                if (!this.name) {
+                    return;
+                }
+                this.searchCharacters(this.name).then(json => {
+                    console.log("Search Characters returned ", json);
+                    this.characterResults = json;
+                });
+
+                this.searchSeries(this.name).then(json => {
+                    console.log("Search Comics returned ", json);
+                    this.comicResults = json;
+                });
 
                 this.name = "";
             }
@@ -179,7 +241,17 @@
             apiKey: "b51a816f55c2b752fe029e625cd2d81b",
             endpoint: "https://gateway.marvel.com",
             name: "Spider",
-            result: {
+            thumbnailSize: {
+                name: "portrait_medium",
+                width: 100,
+                height: 150,
+            },
+            characterResults: {
+                data: {
+                    results: []
+                }
+            },
+            comicResults: {
                 data: {
                     results: []
                 }
