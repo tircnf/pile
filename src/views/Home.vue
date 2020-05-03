f6198e645772548bcc24cfa49a5807ee261dd7f4
mburns <mike.burns@afoundria.com>
2020-05-03 14:39:47 -0500

    Rename HelloWorld to GenericSearch
    Move the contents from Home to a new "Search" view.
    and put some other text on the home screen.
    
    THen went ahead and copied the now renamed GenericSearch component
    on the home screen too (after removing the giant marvel Comics icon).

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,103 @@
 <template>
     <div class="home">
-            <HelloWorld msg="Search for a Marvel character or comic"/>
+<!--            <v-img src="../assets/marvel-banner.jpg"/>-->
+        <v-card
+                xmax-width="400"
+                class="mx-auto"
+        >
+
+            <v-container>
+                <v-row dense>
+
+                    <v-col
+                            cols="12"
+                    >
+                        <v-card
+                                color="blue"
+                                dark
+                                to="search"
+                        >
+                            <div class="d-flex flex-no-wrap xjustify-space-between">
+                                <v-avatar
+                                        class="ma-3 "
+                                        size="125"
+                                        xtile
+
+                                >
+                                    <v-img src="../assets/marvel-banner.jpg"
+                                    />
+                                </v-avatar>
+
+                                <div>
+                                    <v-card-title
+                                            class="headline"
+                                    >
+                                        Marvel Browser
+                                    </v-card-title>
+
+                                    <v-card-subtitle>
+                                        This site is not affiliated with Marvel Comics in any way.
+                                    </v-card-subtitle>
+
+                                    <v-card-text>
+                                        But it does take advantage of
+                                        their fun to use API.
+                                            <span class="mr-2">Data provided by Marvel. &copy; 2020 MARVEL</span>
+                                    </v-card-text>
+                                </div>
+
+
+                            </div>
+                        </v-card>
+                    </v-col>
+
+
+                    <v-col cols="12" sm="6">
+                        <v-card
+                                color="#385F73"
+                                dark
+                                to="character"
+                        >
+                            <v-card-title class="headline">Find your favorite Character</v-card-title>
+
+                            <v-card-subtitle>Search for your favorite character among the hundreds of the Marvel Super Heroes and Super Villans.</v-card-subtitle>
+
+                            <v-card-actions>
+                                <v-btn to="search" text>Search for a Character</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-col>
+
+                    <v-col cols="12" sm="6">
+                        <v-card
+                                color="#385F73"
+                                dark
+                                to="series"
+                        >
+                            <v-card-title class="headline">Find your favorite Comic Book Series</v-card-title>
+
+                            <v-card-subtitle>There are a lot of great comic series out there starting from way back in the 1950's.  Check out 'Journey Into Mystery'</v-card-subtitle>
+
+                            <v-card-actions>
+                                <v-btn to="search" text>Search for a Series</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-col>
+
+                </v-row>
+            </v-container>
+        </v-card>
+
+        <GenericSearch/>
     </div>
 </template>
 
 <script>
     // @ is an alias to /src
-    import HelloWorld from '../components/HelloWorld.vue'
 
+    import GenericSearch from "../components/GenericSearch";
     export default {
         name: 'Home',
-        components: {
-            HelloWorld
-        }
+        components: {GenericSearch}
     }
 </script>
---
+++ src/views/Search.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home">
+        <GenericSearch msg="Search for a Marvel character or comic"/>
+    </div>
+</template>
+
+<script>
+
+
+    import GenericSearch from "../components/GenericSearch";
+    export default {
+        name: "Search",
+        components: {GenericSearch}
+    }
+</script>
+
+<style scoped>
+
+</style>
