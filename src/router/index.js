1e570d15a951731284a243af4262a9df9ad12fc6
mburns <mike.burns@afoundria.com>
2020-05-04 00:58:45 -0500

    Tweak the router to always jump to top.
    feels like this should be the default behavior.  If you use back/forward
    buttons, then savedPosition is sent in, and the position is saved.

--- src/router/index.js
+++ src/router/index.js
@@ -45,7 +45,14 @@ const router = new VueRouter({
     //
     // mode: 'history',
     //base: process.env.BASE_URL,
-    routes
+    routes,
+    scrollBehavior (to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition
+        } else {
+            return { x: 0, y: 0 }
+        }
+    }
 });
 
 export default router
