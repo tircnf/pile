8dff6c73b7396db4938f5d95406b0398f7df8384
mburns <mike.burns@afoundria.com>
2020-05-05 11:23:12 -0500

    tweaks to the ComicList.vue
    the "More" button and the spinner weren't working correctly.
    There is a lot of similarity between comic.title and comic.series.
    But they aren't exact.

--- src/components/ComicList.vue
+++ src/components/ComicList.vue
@@ -4,42 +4,44 @@
             <v-col cols="12">
                 <v-icon v-if="loading">mdi-loading mdi-spin</v-icon>
 
-                <span v-if="results">Showing {{results.offset+1}} - {{results.limit + results.offset}} of {{results.total}}</span>
-                <v-btn v-if="!loading" class="ml-5" @click="$emit('more')">More</v-btn>
+                <div v-if="results && results.total">
+                    <span v-if="results">Showing {{results.offset+1}} - {{results.limit + results.offset}} of {{results.total}}</span>
+                    <v-btn @click="$emit('more')" class="ml-5" v-if="!loading">More</v-btn>
 
-                <v-list v-if="results" shaped>
-                    <v-list-item-group xv-model="selectedCharacter">
-                        <v-list-item :key="comic.id" v-for="(comic,index) in results.results">
-                            <v-list-item-icon>
-                                <!--suppress HtmlUnknownTarget -->
-                                <v-img
-                                        :alt="`thumbnail image for {{comic.name}}`"
-                                        :src="`${comic.thumbnail.path}/portrait_xlarge.jpg`"
-                                        contain
-                                        xheight="140"
-                                        xwidth="190"
-                                />
-                            </v-list-item-icon>
+                    <v-list shaped v-if="results">
+                        <v-list-item-group xv-model="selectedCharacter">
+                            <v-list-item :key="comic.id" v-for="(comic) in results.results">
+                                <v-list-item-icon>
+                                    <!--suppress HtmlUnknownTarget -->
+                                    <v-img
+                                            :alt="`thumbnail image for {{comic.name}}`"
+                                            :src="`${comic.thumbnail.path}/portrait_xlarge.jpg`"
+                                            contain
+                                            xheight="140"
+                                            xwidth="190"
+                                    />
+                                </v-list-item-icon>
 
-                            <v-list-item-content>
-                                <div>
-                                    <h5>{{index+1}} {{comic.title}}  #{{comic.issueNumber}}/{{comic.format}}</h5>
-                                    <p><span v-html="comic.description"/></p>
-                                    <ul class="mb-3">
-                                        <li> Characters: {{comic.characters.available}}</li>
-                                        <li> Events: {{comic.events.available}}</li>
-                                        <li> Series: {{comic.series.available}}</li>
-                                        <li> Stories: {{comic.stories.available}}</li>
-                                        <li> On Sale Date: {{comic.dates[0].date.substr(0,10)}}</li>
-                                    </ul>
-<!--                                    <pre>{{comic}}</pre>-->
-                                </div>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </v-list-item-group>
-                </v-list>
+                                <v-list-item-content>
+                                    <div>
+                                        <h5>{{comic.series.name}} #{{comic.issueNumber}}/{{comic.format}}</h5>
+                                        {{comic.title}}
+                                        <p><span v-html="comic.description"/></p>
+                                        <ul class="mb-3">
+                                            <li> Characters: {{comic.characters.available}}</li>
+                                            <li> Events: {{comic.events.available}}</li>
+                                            <li> Stories: {{comic.stories.available}}</li>
+                                            <li> {{comic.dates[0].date.substr(0,10)}}</li>
+                                        </ul>
+<!--                                        <pre>{{comic}}</pre>-->
+                                    </div>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list-item-group>
+                    </v-list>
+                <v-btn @click="$emit('more')" class="ml-5" v-if="!loading">More</v-btn>
+                </div>
             </v-col>
-            <v-btn v-if="!loading" class="ml-5" @click="$emit('more')">More</v-btn>
         </v-row>
     </div>
 </template>
@@ -67,10 +69,9 @@
                         this.results = null;
                         this.loading = true;
                         this.comicList.then(result => {
-                            console.log("result for comicList = ",result);
+                            console.log("result for comicList = ", result);
                             this.results = result;
-                            // setTimeout(()=>{this.loading=false},1000);
-                            this.loading=false;
+                            this.loading = false;
                         })
                     }
                 }
